e633bf5442902d976cd2403e71022ebc406d8226
Matt Chung <itsmemattchung@gmail.com>
2015-11-20 12:06:38 +0000

    Added add_deployment to speedcurve.py

--- speedcurve/speedcurve.py
+++ speedcurve/speedcurve.py
@@ -12,22 +12,44 @@ class SpeedCurve(SpeedCurveCore):
     def __init__(self, api_key=None):
         super(SpeedCurve, self).__init__({}, api_key=api_key)
 
-    def url(self, id=None, days=30, browser='all'):
-        """Retrieve url specified by id
+    def add_deployment(self, note=None, detail=None):
+        """Add a deployment and trigger round of testing.
 
-        :param int id: (required) id of URL
-        :param int days: (optional) number of days of tests (max: 365)
-        :param string browser: (optional) all, chrome, firefox, ie, or safari
-        :returns: generator of :class:`Url <speedcurve.urls.Url>`
-
+        :param string note: (required) short note used on site
+        :param string detail: (optional) detail to display for more context
+        :returns: :class:`Deployment <speedcurve.deployments.Deployment>`
         """
-        url = self.session.build_url('urls', str(id))
-        params = {
-            'days': days,
-            'browser': browser
+        data = {
+            'note': str(note) or '',
+            'detail': str(detail) or ''
         }
-        json = self._json(self._get(url, params=params))
-        return self._instance_or_null(Url, json)
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        url = self.session.build_url('deploy')
+        json = self._json(self._post(url, data=data, headers=headers), 200)
+        return self._instance_or_null(Deployment, json)
+
+    def get_latest_deployment(self):
+        """Retrieve latest deployment
+
+        :returns: :class:`Deployment <speedcurve.deployments.Deployment>`
+        """
+        url = self.session.build_url('deploy', 'latest')
+        json = self._json(self._get(url), 200)
+        return self._instance_or_null(Deployment, json)
+
+    def get_deployment(self, id=None):
+        """Retrieve a deployment specified by id
+
+        :params int id: (required) id of deployment
+        :returns: :class:`Deployment <speedcurve.deployments.Deployment>`
+        """
+        url = self.session.build_url('deploy', str(id))
+        json = self._json(self._get(url), 200)
+        return self._instance_or_null(Deployment, json)
 
     def sites(self):
         """Retrieve all sites for account"""
@@ -46,29 +68,19 @@ class SpeedCurve(SpeedCurveCore):
         json = self._json(self._get(url), 200)
         return self._instance_or_null(Test, json)
 
-    def get_latest_deployment(self):
-        """Retrieve latest deployment
+    def url(self, id=None, days=30, browser='all'):
+        """Retrieve url specified by id
 
-        :returns: :class:`Deployment <speedcurve.deployments.Deployment>`
+        :param int id: (required) id of URL
+        :param int days: (optional) number of days of tests (max: 365)
+        :param string browser: (optional) all, chrome, firefox, ie, or safari
+        :returns: generator of :class:`Url <speedcurve.urls.Url>`
+
         """
-        url = self.session.build_url('deploy', 'latest')
-        json = self._json(self._get(url), 200)
-        return self._instance_or_null(Deployment, json)
-
-    def get_deployment(self, id=None):
-        """Retrieve a deployment specified by id
-
-        :params int id: (required) id of deployment
-        :returns: :class:`Deployment <speedcurve.deployments.Deployment>`
-        """
-        url = self.session.build_url('deploy', int(id))
-        json = self._json(self._get(url), 200)
-        return self._instance_or_null(Deployment, json)
-
-    def add_deployment(self, note=None, detail=None):
-        """Add a deployment and trigger round of testing.
-
-        :param string note: (required) short note used on site
-        :param string detail: (optional) detail to display for more context
-        """
-        pass
+        url = self.session.build_url('urls', str(id))
+        params = {
+            'days': days,
+            'browser': browser
+        }
+        json = self._json(self._get(url, params=params))
+        return self._instance_or_null(Url, json)
