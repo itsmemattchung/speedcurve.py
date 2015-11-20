6c69920db26a973135c652c8ceb51210b008671d
Matt Chung <itsmemattchung@gmail.com>
2015-11-20 20:49:50 +0000

    Added flake8 to tox.ini and fixed __init__.py to be compliant

--- speedcurve/__init__.py
+++ speedcurve/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from .speedcurve import SpeedCurve
 
 __author__ = 'Matt Chung'
@@ -5,3 +6,7 @@ __copyright__ = 'Copyright 2014 Matt Chung'
 __license__ = 'MIT'
 __title__ = 'speedcurve'
 __version__ = '0.1.0'
+
+__all__ = (
+    'SpeedCurve',
+)
